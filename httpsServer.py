49d998e764dada12e5fc96c68e9da9a8fdb6906b
ecspress <ecspress@hotmail.com>
2022-09-29 16:05:27 +0500

    initial work done for demo

--- httpsServer.py
+++ httpsServer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
-import BaseHTTPServer, SimpleHTTPServer
+# import BaseHTTPServer, SimpleHTTPServer
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 import ssl
 
-httpd = BaseHTTPServer.HTTPServer(('localhost', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd = HTTPServer(('localhost', 4443), SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
 httpd.serve_forever()
